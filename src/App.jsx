bd4971eba0bf7a568407afd6a87b1486ea10d7b9
ilechang <117113333+ilechang@users.noreply.github.com>
2025-09-03 18:13:05 -0400

    123

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Experience from "./components/Experience";
 import Configurator from "./components/Configurator";
 import { CustomizationProvider } from "./contexts/Customization";
 
+import React, { Suspense } from "react";
+
 function App() {
   return (
     <CustomizationProvider>
